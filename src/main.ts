704b519881cabe430820c83eb6e1e23e7c006ffa
maziac <thomas.busse@gmx.net>
2021-02-26 23:49:33 +0100

    Cleanup.

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { Log, TestAdapterRegistrar } from 'vscode-test-adapter-util';
 import { Z80UnitTestAdapter } from './adapter';
 
 export async function activate(context: vscode.ExtensionContext) {
-
-	const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
-
 	// Create a simple logger that can be configured with the configuration variables
 	// `z80UnitTestExplorer.logpanel` and `z80UnitTestExplorer.logfile`
+	let workspaceFolder;
+	if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0)
+		workspaceFolder = vscode.workspace.workspaceFolders[0];
 	const log = new Log('z80UnitTestExplorer', workspaceFolder, 'Z80 Unit Test Explorer Log');
 	context.subscriptions.push(log);
 
-	// get the Test Explorer extension
+	// Get the Test Explorer extension
 	const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
 	if (log.enabled) log.info(`Test Explorer ${testExplorerExtension ? '' : 'not '}found`);
 
@@ -20,7 +20,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
 		const testHub = testExplorerExtension.exports;
 
-		// this will register an ExampleTestAdapter for each WorkspaceFolder
+		// This will register an ExampleTestAdapter for each WorkspaceFolder
 		context.subscriptions.push(new TestAdapterRegistrar(
 			testHub,
 			workspaceFolder => new Z80UnitTestAdapter(workspaceFolder, log),
